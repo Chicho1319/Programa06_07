5acbb03b0d58512d3e6f5966fee11d0aec7c1c95
Cesar Agusto Chaparro Conde <cesarelchid@hotmail.com>
2020-12-10 20:39:41 -0600

    Descripción

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Programa06_07
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Calcula impuesto ambiental
+
+            // Variables
+            int opcion = 0;
+            int fabricacion = 0;
+            string dato = "";
+
+            // Pedimos el tipo de vehiculo 
+            Console.WriteLine("1. Auto, 2. Motocicleta, 3. Bicicleta");
+            dato = Console.ReadLine();
+            opcion = Convert.ToInt32(dato);
+
+            // Pedimos cuando se fabrico
+            Console.WriteLine("¿Cuándo se fabricó?");
+            dato = Console.ReadLine();
+            fabricacion = Convert.ToInt32(dato);
+
+            // Verificamos las opciones
+
+            if (opcion == 1)
+            {
+                if (fabricacion <= 1980)
+                    Console.WriteLine("El impuesto del auto es 8%");
+                if (fabricacion > 1980 && fabricacion < 2000)
+                    Console.WriteLine("El impuesto del auto es 5%");
+                if (fabricacion >= 2000)
+                    Console.WriteLine("El impuesto del auto es 3%");
+            }
+
+            if (opcion == 2)
+            {
+                if (fabricacion <= 1980)
+                    Console.WriteLine("El impuesto de la motocicleta es 4%");
+                if (fabricacion > 1980 && fabricacion < 2000)
+                    Console.WriteLine("El impuesto de la motocicleta es 2%");
+                if (fabricacion >= 2000)
+                    Console.WriteLine("El impuesto de la motocicleta es 1%");
+            }
+
+            if (opcion == 3)
+                Console.WriteLine("La bicicleta no paga impuesto");
+
+            Console.ReadKey();
+        }
+    }
+}
